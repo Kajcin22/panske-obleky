3a278d068c039c0b07fb44eec6dac02e5829ac30
Kajcin22 <masekkaterina@gmail.com>
2022-04-19 20:10:59 +0200

    košile pozlacena

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('funguju');
+const productImg = document.querySelector('#product-image');
+const changeColor = (event) => {
+  productImg.classList.toggle('toggle-color');
+};
+productImg.addEventListener('click', changeColor);
